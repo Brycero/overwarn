666f83d09fe8193369b29434ec3f87ffa709ccff
Brycero <13734212+Brycero@users.noreply.github.com>
2026-07-21 07:44:57 -0500

    chore: workers migration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 import packageJson from "./package.json";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   env: {
     NEXT_PUBLIC_APP_VERSION: packageJson.version,
   },
